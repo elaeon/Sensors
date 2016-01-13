e0c1584046948914d5715ed596b1662413da47ab
elaeon <mara80@gmail.com>
2016-01-13 14:11:09 -0600

    Create sensor_sync.py

---
+++ sensor_sync.py
@@ -0,0 +1,73 @@
+import time
+import socket
+import logging
+
+from queuelib import FifoDiskQueue
+
+
+class SyncData(object):
+    def __init__(self, name, carbon_server, carbon_port=2003, delay=1, 
+                delay_error_sensor=0.2, delay_error_connection=2):
+        self.name = name
+        self.DELAY = delay
+        self.DELAY_ERROR_SENSOR = delay_error_sensor
+        self.DELAY_ERROR_CONNECTION = delay_error_connection
+        self.CARBON_SERVER = carbon_server #'192.168.52.50'
+        self.CARBON_PORT = carbon_port
+
+    def logging_setup(self):
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        logger = logging.getLogger(self.name)
+        hdlr = logging.FileHandler('{}.log'.format(self.name))
+        hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr)
+        logger.setLevel(logging.INFO)
+        return logger
+
+    def send_msg(self, message):
+        sock = socket.socket()
+        try:
+            sock.connect((self.CARBON_SERVER, self.CARBON_PORT))
+            sock.sendall(message)
+        except socket.error:
+            #logger.info("No se puede conectar a carbon {}".format(len(data)))
+            return False
+        except TypeError:
+            return True
+        else:
+            return True
+        finally:
+            sock.close()
+
+    def send_blocks_msg(self, messages):
+        sock = socket.socket()
+        try:
+            sock.connect((self.CARBON_SERVER, self.CARBON_PORT))
+            for message in messages:
+                sock.sendall(message)
+        except socket.error:
+            #logger.info("No se puede conectar a carbon {}".format(len(data)))
+            return False
+        else:
+            return True
+        finally:
+            sock.close()
+
+    def run(self):
+        while True:
+            queue = FifoDiskQueue("{}.fifo.sql".format(self.name))
+            print(len(queue))
+            message = queue.pop()
+            while len(queue) >= 20:
+                messages = [message]
+                while len(messages) <= 19 and message is not None:
+                    messages.append(queue.pop())
+                if not self.send_blocks_msg(messages):
+                    for message in messages:
+                        queue.push(message)
+                    break
+            else:
+                if not self.send_msg(message):
+                    queue.push(message)
+            queue.close()
+            time.sleep(self.DELAY)
