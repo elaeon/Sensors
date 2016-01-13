84b8e532353e36aa49076268c552e5a58f6829ca
elaeon <mara80@gmail.com>
2016-01-13 11:20:39 -0600

    Update sensor_writer.py

--- sensor_writer.py
+++ sensor_writer.py
@@ -6,14 +6,20 @@ class WriterData(object):
     def __init__(self, sensor_name):
         self.node = platform.node().replace('.', '-')
         self.sensor_name = sensor_name
+        self.database_name = "{}.fifo.sql".format(self.sensor_name)
 
-    def msg_format(self, v, timestamp):
-        return "system.{}.{} {} {}\n".format(self.node, self.sensor_name, v, timestamp)
+    def msg_format(self, message):
+        if len(message) == 2:
+            v, timestamp = message
+            return "system.{}.{} {} {}\n".format(self.node, self.sensor_name, v, timestamp)
+        elif len(message) == 3:
+            v, timestamp, name = message
+            return "system.{}.{} {} {}\n".format(self.node, name, v, timestamp)
 
-    def run(self, database_name, num_messages_second, messages_fn, sleep=1):
+    def run(self, num_messages_second, messages_fn, sleep=1):
         while True:
-            queue = FifoDiskQueue(database_name)
-            for t, timestamp in messages_fn(self.node, num_messages_second):
-                queue.push(self.msg_format(t, timestamp))
+            queue = FifoDiskQueue(self.database_name)
+            for message in messages_fn(self.node, num_messages_second):
+                queue.push(self.msg_format(message))
                 time.sleep(sleep)
             queue.close()
