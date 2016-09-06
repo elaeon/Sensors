fe955e5f760e62836905f63b79c5b29030678b8d
Alejandro Martínez Romero <mara80@gmail.com>
2016-09-06 17:08:16 -0500

    Update build.py

--- buider/build.py
+++ buider/build.py
@@ -1,11 +1,14 @@
-from fabric.api import run, local, env, cd, put
+from fabric.api import run, local, env, cd, put, sudo
 
 env.user = "pi"
 
 def config_hostname():
     hostname = raw_input("Hostname: ")
-    command = "sudo echo '{hostname}' > /etc/hostname".format(hostname=hostname)
-    run(command)
+    command = "echo '{hostname}' > /etc/hostname".format(hostname=hostname)
+    sudo(command)
+
+    command_1 = "echo '127.0.0.1\t{hostname}' >> /etc/hosts".format(hostname=hostname)    
+    sudo(command_1)
 
 def install_git():   
     run("sudo apt-get install git")
@@ -20,6 +23,28 @@ def supervisor_conf():
 def install_sensors():
     with cd('/var/'):
         run("sudo git clone https://github.com/elaeon/sensors.git")
+
+def install_ds18b20():
+    command = "echo 'dtoverlay=w1-gpio' >> /boot/config.txt"
+    sudo(command)
+
+def modprobes_enable():
+    command_1 = "modprobe w1-gpio"
+    command_2 = "modprobe w1-therm"
+    sudo(command_1)
+    sudo(command_2)
+
+def nombre_sensor():
+    command_1 = "ls /sys/bus/w1/devices"
+    resultado = run(command_1)
+    print resultado.split(" ")[0]
+
+def install_bibliotecas_humedad():
+    sudo("apt-get install build-essential python-dev")
+    with cd('/home/pi/'):
+        run("git clone https://github.com/adafruit/Adafruit_Python_DHT")
+    with cd('/home/pi/Adafruit_Python_DHT'):
+        sudo("python setup.py install")
 
 
 def install():
